ef1fbac68bfb852f55491778a295015d8ab6c245
Swathi Jettiboina <110420243+Swathijetti2004@users.noreply.github.com>
2024-07-30 00:22:57 +0530

    Time: 17 ms (84.30%), Space: 56.2 MB (48.09%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        // ArrayList<String> arr=new ArrayList<>();
+        // for(int val:nums){
+        //     arr.add(String.toString(val));
+        // }
+        // Collections.sort(arr);
+        // int[] num=new int[nums.length];
+        // for(String val:nums.toArray()){
+        //     arr.add(String.toString(val));
+        // }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
