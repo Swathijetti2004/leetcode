615775909111fbfceb9065f4048c1e21dcbd8d92
Swathi Jettiboina <110420243+Swathijetti2004@users.noreply.github.com>
2024-08-05 14:37:01 +0530

    Time: 50 ms (6.03%), Space: 16.7 MB (27.92%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        s={}
+        for char in text:
+            if char not in s:
+                s[char]=1
+            else:
+                s[char]+=1
+        count=0
+        while("b" in s.keys()):
+            for char in "balloon":
+                if char not in s.keys() or  s[char]==0:
+                    break
+                else:
+                    s[char]-=1
+                    if char=='n':
+                        count+=1
+                if s[char]==0:
+                    s.pop(char)
+        return count
+                    
+        
