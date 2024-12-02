c2ad2eb52889f933bdce5e00977fcac323d85640
Swathi Jettiboina <110420243+Swathijettiboina@users.noreply.github.com>
2024-12-02 09:40:47 +0530

    Time: 0 ms (100.00%), Space: 45.8 MB (53.71%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,39 +1,68 @@
 class Solution {
-    public int[] searchRange(int[] v, int x) {
-         // Your code goes here
-           int l=0;
-           int r=v.length-1;
-           int first=-1;
-           int last=-1;
+    public int[] searchRange(int[] arr, int x) {
+        
+        int start=0;
+        int end=arr.length-1;
+        int first=-1;
+        int last=-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(x==arr[mid]){
+                first=mid;
+                end=mid-1;
+            }else if(x<arr[mid]){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        start=0;
+        end=arr.length-1;
+                while(start<=end){
+            int mid=(start+end)/2;
+            if(x==arr[mid]){
+                last=mid;
+                start=mid+1;
+            }else if(x<arr[mid]){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+//          // Your code goes here
+//            int l=0;
+//            int r=v.length-1;
+//            int first=-1;
+//            int last=-1;
            
-           //first occurance
-           while(l<=r){
-               int mid=(l+r)/2;
-               if(v[mid]==x){
-                   first=mid;
-                   r=mid-1;
-               }else if(v[mid]>x){
-                   r=mid-1;
-               }else{
-                   l=mid+1;
-               }
-           }
+//            //first occurance
+//            while(l<=r){
+//                int mid=(l+r)/2;
+//                if(v[mid]==x){
+//                    first=mid;
+//                    r=mid-1;
+//                }else if(v[mid]>x){
+//                    r=mid-1;
+//                }else{
+//                    l=mid+1;
+//                }
+//            }
            
-           l=0;
-           r=v.length-1;
-           //last occurance
-           while(l<=r){
-               int mid=(l+r)/2;
-               if(v[mid]==x){
-                   last=mid;
-                   l=mid+1;
-               }else if(v[mid]>x){
-                   r=mid-1;
-               }else{
-                   l=mid+1;
-               }
-           }
-        int arr[]={first,last};
-        return arr;
+//            l=0;
+//            r=v.length-1;
+//            //last occurance
+//            while(l<=r){
+//                int mid=(l+r)/2;
+//                if(v[mid]==x){
+//                    last=mid;
+//                    l=mid+1;
+//                }else if(v[mid]>x){
+//                    r=mid-1;
+//                }else{
+//                    l=mid+1;
+//                }
+//            }
+        int ans[]={first,last};
+        return ans;
     }
 }
